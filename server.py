cc63018c320bb5c74740ac87895b8bf21f14ecb4
Sergio Inzunza <inxunxa@gmail.com>
2022-02-15 20:46:27 -0700

    FSDI 108 #4

--- server.py
+++ server.py
@@ -1,7 +1,8 @@
-from flask import Flask, abort
+from flask import Flask, abort, request
 from mock_data import catalog
 from about_me import me, test
 import json
+import random
 
 # create the server/app
 app = Flask("server")
@@ -40,6 +41,31 @@ def test():
 def get_catalog():
     return json.dumps(catalog)
 
+
+@app.route("/api/catalog", methods=["POST"])
+def save_product():
+    product = request.get_json()  # read the payload as a dictionary from json string
+
+    # validate
+    # title and longer than 5 chars
+    if not "title" in product or len(product["title"]) < 5:
+        return abort(400, "There should be a title. Title should be at least 5 chars long.")
+
+    # there should be a price
+    if not "price" in product:
+        return abort(400, "Price is required.")
+
+    # if the price is not and int AND not a float, error
+    if not isinstance(product["price"], int) and not isinstance(product["price"], float):
+        return abort(400, "Price is invalid.")
+
+    # the price should be greater than zero
+    if product["price"] <= 0:
+        return abort(400, "Price should be greater than zero.")
+
+    product["_id"] = random.randint(10000, 50000)
+    catalog.append(product)  # save it
+    return json.dumps(product)
 
 
 # get /api/catalog/count
@@ -86,6 +112,82 @@ def get_most_expensive():
             pivot = prod
     
     return json.dumps(pivot)
+
+
+# get /api/categories
+# return a list of strings, representing the UNIQUE categories
+
+@app.route("/api/categories")
+def get_categories():
+
+    res = []
+    for prod in catalog:
+        category = prod["category"]
+
+        # if category not exist inside res
+        # push category into res
+        if not category in res:
+            res.append(category)
+
+    return json.dumps(res)
+
+
+# create an endpoint that allow the client (react) to retrieve
+# all the products that belongs to and specified category
+# the client will the send the category and expect a list of products in return
+
+
+# ? what's the ULR ????    /api/catalog/<category>
+@app.route("/api/catalog/<category>")
+def products_by_category(category):
+    res = []
+    for prod in catalog:
+        if prod["category"] == category:
+            res.append(prod)
+
+    return json.dumps(res)
+
+
+#############################################################
+########### API Methods for Coupon codes ####################
+#############################################################
+coupons = []
+
+# {
+#  code: "qwerty"
+#  discount: 10
+# }
+
+# get all       get /api/coupons
+# save new      post  /api/coupons
+# get by code   get  /api/coupons/<code>
+
+
+@app.route("/api/coupons")
+def get_coupons():
+    return json.dumps(coupons)
+
+
+@app.route("/api/coupons", methods=["POST"])
+def save_coupon():
+    coupon = request.get_json()
+
+    # validation
+
+    coupon["_id"] = random.randint(500, 900)
+    coupons.append(coupon)
+
+    return json.dumps(coupon)
+
+
+@app.route("/api/coupons/<code>")
+def get_coupon_by_code(code):
+    for coupon in coupons:
+        if coupon["code"] == code:
+            return json.dumps(coupon)
+
+    return abort(404)
+
 
 
 # start the server
